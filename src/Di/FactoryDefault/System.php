c184a0b5a80cb95997ffd3667fc94242d6e4fac3
Phoenix Osiris <blogger@twistersfury.com>
2016-08-06 19:43:24 -0400

    Debug Stuff

--- src/Di/FactoryDefault/System.php
+++ src/Di/FactoryDefault/System.php
@@ -115,7 +115,7 @@
                     $simpleView = $this->get('\Phalcon\Mvc\View\Simple');
 
                     $simpleView->setViewsDir($this->getPathManager()->getThemesDir() . '/default');
-                    $simpleView->registerEngines(['volt' => '\TwistersFury\Phalcon\Core\Mvc\View\Engine\Volt']);
+                    $simpleView->registerEngines(['.volt' => '\TwistersFury\Phalcon\Core\Mvc\View\Engine\Volt']);
 
                     return $simpleView;
                 }
